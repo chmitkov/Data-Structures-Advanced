9452c77382e189b693336fe20157865cd0e8a6ca
ch.mitkov <ch.mitko@gmail.com>
2021-08-17 15:38:30 +0300

    Add second part of exercise

--- 02.B-Trees_2-3Trees_And_AVLTrees_Exercises/src/main/java/AVL.java
+++ 02.B-Trees_2-3Trees_And_AVLTrees_Exercises/src/main/java/AVL.java
@@ -26,11 +26,57 @@ public class AVL<T extends Comparable<T>> {
     }
 
     public void delete(T item) {
-        throw new UnsupportedOperationException();
+        this.root = delete(this.root, item);
+
+    }
+
+    private Node<T> delete(Node<T> node, T item) {
+        if (node == null) {
+            return null;
+        }
+
+        int compare = item.compareTo(node.value);
+
+        if (compare < 0) {
+            node.left = delete(node.left, item);
+        } else if (compare > 0) {
+            node.right = delete(node.right, item);
+        } else {
+            if (node.left == null) {
+                return node.right;
+            }
+
+            if (node.right == null) {
+                return node.left;
+            }
+
+            Node<T> rightMin = new Node<>(getMin(node.right));
+
+            rightMin.left =node.left;
+            rightMin.right =  this.deleteMin(node.right);
+            node = rightMin;
+        }
+
+        updateHeight(node);
+        return balance(node);
     }
 
     public void deleteMin() {
-        throw new UnsupportedOperationException();
+       this.root =  deleteMin(this.root);
+    }
+
+    private Node<T> deleteMin(Node<T> node) {
+        if(node == null){
+            return null;
+        }
+
+        if(node.left == null){
+            return  node.right;
+        }
+
+        node.left = deleteMin(node.left);
+        updateHeight(node);
+        return balance(node);
     }
 
     public void deleteMax() {
@@ -64,13 +110,13 @@ public class AVL<T extends Comparable<T>> {
         return balance(node);
     }
 
-    private Node<T> getMin(Node<T> node) {
+    private T getMin(Node<T> node) {
         if (node == null) {
             return null;
         }
 
         if (node.left == null) {
-            return node;
+            return node.value;
         }
 
         return getMin(node.left);
@@ -101,7 +147,7 @@ public class AVL<T extends Comparable<T>> {
     private Node<T> balance(Node<T> node) {
         int balance = this.balanceFactor(node);
 
-        if (balance < - 1) {
+        if (balance < -1) {
             int childBalance = this.balanceFactor(node.right);
             if (childBalance > 0) {
                 node.right = rotateRight(node.right);
@@ -133,6 +179,7 @@ public class AVL<T extends Comparable<T>> {
 
         return node;
     }
+
     private int balanceFactor(Node<T> node) {
         return height(node.left) - height(node.right);
     }
--- 02.B-Trees_2-3Trees_And_AVLTrees_Exercises/src/main/java/TwoThreeTree.java
+++ 02.B-Trees_2-3Trees_And_AVLTrees_Exercises/src/main/java/TwoThreeTree.java
@@ -1,3 +1,5 @@
+import com.sun.source.tree.Tree;
+
 public class TwoThreeTree<K extends Comparable<K>> {
     private TreeNode<K> root;
 
@@ -11,6 +13,22 @@ public class TwoThreeTree<K extends Comparable<K>> {
 
         private TreeNode(K key) {
             this.leftKey = key;
+        }
+
+        public TreeNode(K root, K leftValue, K rightValue) {
+            this(root);
+            this.leftChild = new TreeNode<>(leftValue);
+            this.rightChild = new TreeNode<>(rightValue);
+
+        }
+
+        public TreeNode(K root, TreeNode<K> left, TreeNode<K> right) {
+
+            this.leftKey = root;
+
+            this.leftChild = left;
+            this.rightChild = right;
+
         }
 
         boolean isThreeNode() {
@@ -27,7 +45,93 @@ public class TwoThreeTree<K extends Comparable<K>> {
     }
 
     public void insert(K key) {
+        if (this.root == null) {
+            this.root = new TreeNode<>(key);
+        } else {
 
+            TreeNode<K> newRoot = insert(this.root, key);
+
+            if (newRoot != null) {
+                this.root = newRoot;
+            }
+        }
+    }
+
+    private TreeNode<K> insert(TreeNode<K> node, K key) {
+
+        if (node == null) return new TreeNode<>(key);
+
+        if (node.isLeaf()) {
+            if (node.isTwoNode()) {
+                if (node.leftKey.compareTo(key) < 0) {
+                    node.rightKey = key;
+                } else {
+                    node.rightKey = node.leftKey;
+                    node.leftKey = key;
+                }
+
+                return null;
+            } else {
+                K left = node.leftKey;
+                K middle = key;
+                K right = node.rightKey;
+                if (node.leftKey.compareTo(key) > 0) {
+                    left = key;
+                    middle = node.leftKey;
+                } else if (node.rightKey.compareTo(key) < 0) {
+                    middle = node.rightKey;
+                    right = key;
+                }
+
+                return new TreeNode<>(middle, left, right);
+            }
+        }
+
+        TreeNode<K> toFix = null;
+        if (node.leftKey.compareTo(key) > 0) {
+            toFix = insert(node.leftChild, key);
+        } else if (node.isTwoNode() && node.leftKey.compareTo(key) < 0) {
+            toFix = insert(node.rightChild, key);
+        } else if (node.isThreeNode() && node.rightKey.compareTo(key) < 0) {
+            toFix = insert(node.rightChild, key);
+        } else { // 3 node, not less than left or greater than right
+            toFix = insert(node.middleChild, key);
+        }
+
+        if (toFix == null) return null;
+
+        if (node.isTwoNode()) {
+            if (node.leftKey.compareTo(toFix.leftKey) < 0) {
+                node.rightKey = toFix.leftKey;
+                node.middleChild = toFix.leftChild;
+                node.rightChild = toFix.rightChild;
+            } else {
+                node.rightKey = node.leftKey;
+                node.leftKey = toFix.leftKey;
+                node.leftChild = toFix.leftChild;
+                node.middleChild = toFix.rightChild;
+            }
+
+            return null;
+        }
+
+        TreeNode<K> newNode;
+        if (node.leftKey.compareTo(key) > 0) {
+            K newKey = node.leftKey;
+            node.leftKey = node.rightKey;
+            newNode = new TreeNode<>(newKey, toFix, node);
+        } else if (node.rightKey.compareTo(key) < 0) {
+            K newKey = node.rightKey;
+            node.rightKey = null;
+            newNode = new TreeNode<>(newKey, node, toFix);
+        } else {
+            TreeNode<K> newLeft = new TreeNode<>(node.leftKey, node.leftChild, toFix.leftChild);
+            TreeNode<K> newRight = new TreeNode<>(node.rightKey, toFix.rightChild, node.rightChild);
+
+            newNode = new TreeNode<>(toFix.leftKey, newLeft, newRight);
+        }
+
+        return newNode;
     }
 
 
